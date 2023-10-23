9f5f90d22a154b1f679d1eca311ab455a554ca03
Burak Can GORGULU <burak.gorgulu@beymen.com>
2023-10-23 16:07:39 +0300

    Satellite Explorer navigation added  main activity

--- app/src/main/java/com/bcan/satelliteexplorer/MainActivity.kt
+++ app/src/main/java/com/bcan/satelliteexplorer/MainActivity.kt
@@ -6,41 +6,24 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.bcan.satelliteexplorer.presentation.util.navigation.SatellitesNavigation
 import com.bcan.satelliteexplorer.ui.theme.SatelliteExplorerTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SatelliteExplorerTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    SatellitesNavigation()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SatelliteExplorerTheme {
-        Greeting("Android")
     }
 }
