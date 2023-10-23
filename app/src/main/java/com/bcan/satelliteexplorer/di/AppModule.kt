70098a7a0b9f294954ac1dae45631024cc5c2f3f
Burak Can GORGULU <burak.gorgulu@beymen.com>
2023-10-24 01:04:53 +0300

    Satellite Explorer added Singleton Annotation

--- app/src/main/java/com/bcan/satelliteexplorer/di/AppModule.kt
+++ app/src/main/java/com/bcan/satelliteexplorer/di/AppModule.kt
@@ -6,13 +6,16 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
     @Provides
+    @Singleton
     fun provideContext(@ApplicationContext context: Context): Context {
         return context
     }
+
 }
