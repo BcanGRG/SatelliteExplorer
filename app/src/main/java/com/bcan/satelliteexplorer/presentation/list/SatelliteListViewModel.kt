bfc138475bab47fe8b0f55ba1dd18e71abe1f9c5
Burak Can GORGULU <burak.gorgulu@beymen.com>
2023-10-23 16:09:06 +0300

    Satellite Explorer added search

--- app/src/main/java/com/bcan/satelliteexplorer/presentation/list/SatelliteListViewModel.kt
+++ app/src/main/java/com/bcan/satelliteexplorer/presentation/list/SatelliteListViewModel.kt
@@ -7,9 +7,14 @@ import com.bcan.satelliteexplorer.data.repository.SatelliteRepository
 import com.bcan.satelliteexplorer.data.util.FileResult
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -17,7 +22,6 @@ import javax.inject.Inject
 data class SatelliteListUiState(
     val isLoading: Boolean = false,
     val errorMessage: String? = null,
-    val list: List<SatelliteResponseModel>? = null
 )
 
 @HiltViewModel
@@ -25,10 +29,32 @@ class SatelliteListViewModel @Inject constructor(
     private val repository: SatelliteRepository
 ) : ViewModel() {
 
-
     private val _uiState: MutableStateFlow<SatelliteListUiState> =
         MutableStateFlow(SatelliteListUiState())
     val uiState: StateFlow<SatelliteListUiState> = _uiState.asStateFlow()
+
+    private val _searchText = MutableStateFlow("")
+    val searchText = _searchText.asStateFlow()
+
+    private val _satellites: MutableStateFlow<List<SatelliteResponseModel>?> =
+        MutableStateFlow(emptyList())
+    val satellites = _searchText
+        .debounce(500L)
+        .onEach { _uiState.update { state -> state.copy(isLoading = true) } }
+        .combine(_satellites) { text, satellites ->
+            if (text.isBlank()) {
+                satellites
+            } else {
+                //delay(3000L) // isLoading durumunun gözükmesini kontrol etmek için aktif edilebilir
+                satellites?.filter { it.doesMatchSearchQuery(text.trim()) }
+            }
+        }
+        .onEach { _uiState.update { state -> state.copy(isLoading = false) } }
+        .stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5000),
+            _satellites.value
+        )
 
     init {
         getSatelliteList()
@@ -52,9 +78,9 @@ class SatelliteListViewModel @Inject constructor(
                             state.copy(
                                 isLoading = false,
                                 errorMessage = null,
-                                list = result.data
                             )
                         }
+                        _satellites.value = result.data
                     }
 
                     is FileResult.OnError -> {
@@ -68,5 +94,9 @@ class SatelliteListViewModel @Inject constructor(
                 }
             }
         }
+    }
+
+    fun onSearchTextChange(text: String) {
+        _searchText.value = text
     }
 }
